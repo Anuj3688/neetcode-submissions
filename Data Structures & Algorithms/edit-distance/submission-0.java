73a007fcb8f8cdc996af54378adbde55d865dfea
Anuj Tiwari <78018206+Anuj3688@users.noreply.github.com>
2026-05-01 19:52:17 +0530

    Add: edit-distance - submission-0

---
+++ Data Structures & Algorithms/edit-distance/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minChange(String word1 , String word2 , int i1 , int i2, int[][] dp){
+        //min number of change
+        // so we have 3 choice and based on that we need to move forward.
+        // in all there we can do any and see which operation works best 1 by 1
+
+        //base case
+        if (i2 == word2.length() && i1 == word1.length()) return 0; 
+
+        if (dp[i1][i2] != -1){
+            return dp[i1][i2];
+        }
+        if (i2 < word2.length() && i1 < word1.length() && word1.charAt(i1) == word2.charAt(i2)){
+            // since it is a match no operation needed moving for next character
+            return dp[i1][i2] = minChange(word1, word2 , i1+1 , i2+1, dp);
+        }
+        //now since it is not match we have 3 choice so will try them all
+        // check also need to be added
+        int choiceIn = Integer.MAX_VALUE;
+        int choiceRe = Integer.MAX_VALUE;
+        int choiceDe = Integer.MAX_VALUE;
+
+        if (i2<word2.length())
+            choiceIn = 1 + minChange(word1 , word2 , i1,  i2+1 ,dp);
+        if (i2<word2.length() && i1< word1.length())
+            choiceRe = 1 + minChange(word1 , word2 , i1+1,  i2+1 , dp);
+        if (i1< word1.length())
+            choiceDe = 1 + minChange(word1 , word2 , i1+1,  i2, dp);
+
+        return dp[i1][i2]= Math.min(choiceIn , Math.min(choiceRe , choiceDe));
+    }
+    public int minDistance(String word1, String word2) {
+        //now dp it
+        int[][] dp = new int[word1.length()+1][word2.length()+1];
+        for (int[] arr : dp) Arrays.fill(arr,-1);
+        return minChange(word1,word2,0,0 , dp);
+    }
+}
